abc82f02d8c5ec3725af6ccb53ff0e58409f139a
Daniel Cunha <danielcunha54@gmail.com>
2020-06-02 23:00:11 -0300

    Setup linting and routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,18 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import chalk from 'chalk';
+import routes from './routes';
 
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(morgan(process.env.MORGAN_MODE || 'dev'));
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${chalk.green(port)}`);
+});
